2d9286c83a3db21c2bc1639642ae364b7a459e23
Raj Randev <40559725+randev85@users.noreply.github.com>
2019-08-15 14:47:17 +0100

    Initial commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+export class AddTodo extends Component {
+    
+    state ={
+        title:'',
+
+    }
+
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: ''});
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.onSubmit} style={{ display: 'flex'}}>
+                <input 
+                    type="text" 
+                    name="title" 
+                    id="" 
+                    style={{flex: '10', padding: '10px'}}
+                    placeholder="Add Item..."
+                    value={this.state.title} 
+                    onChange={this.onChange} 
+                />
+                <input 
+                    type="submit" 
+                    value="Submit" 
+                    className="btn"
+                    style={{flex: '1'}}
+                />
+            </form>
+        ) 
+    }    
+}
+
+AddTodo.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+export default AddTodo
